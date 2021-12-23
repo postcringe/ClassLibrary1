c3c33de497109e74144b32731cfc3c2af873b583
postcringe <surohoc@mail.ru>
2021-12-24 01:01:14 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Cl
+    {
+      public  double sum(int a, int b,int c ,int x)
+        {
+            if (x<0 && b!=0)
+            return a*x*x+b;
+
+            if (x > 0 && b == 0)
+                return (x-a)/(x-c);
+            return x / c;
+
+        }
+    }
+}
